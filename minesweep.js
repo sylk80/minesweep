f9fcf35dabdcd3fa582d135491e9fad0f8b1ab1e
Szilvia Mezo <Szilvia.Mezo@ibm.com>
2021-07-12 11:24:29 +0200

    fix: display output

--- minesweep.js
+++ minesweep.js
@@ -1,5 +1,15 @@
 const minesweeper = () => {
-    return null
+    const userTable = [
+        [" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "],
+    ]
+
+    const drawTable = (table) => {
+        return table
+    }
+
+    return drawTable(userTable)
 }
 
 module.exports = minesweeper
