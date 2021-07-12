cab4b8e958bbd399fca85d89e6d6e61b78838f81
Szilvia Mezo <Szilvia.Mezo@ibm.com>
2021-07-12 11:29:47 +0200

    fix: display empty table

--- minesweep.js
+++ minesweep.js
@@ -1,15 +1,38 @@
 const minesweeper = () => {
+    const cellStart = "|"
     const userTable = [
         [" ", " ", " "],
         [" ", " ", " "],
         [" ", " ", " "],
     ]
 
-    const drawTable = (table) => {
-        return table
+    const drawUserTable = (table) => {
+        let drawnTable = addNewRow()
+        for (let indexY = 0; indexY < 3; indexY++) {
+            drawnTable = drawRows(table, indexY, drawnTable)
+        }
+        return drawnTable
     }
 
-    return drawTable(userTable)
+    const drawRows = (table, indexY, drawnTable) => {
+        drawnTable += cellStart
+        for (let indexX = 0; indexX < 3; indexX++) {
+            drawnTable += table[indexY][indexX]
+            drawnTable += addCellEnding(indexX)
+        }
+        drawnTable += addNewRow()
+        return drawnTable
+    }
+
+    const addNewRow = () => {
+        return "+-+-+-+\n"
+    }
+
+    const addCellEnding = (indexX) => {
+        return indexX < 2 ? cellStart : cellStart + "\n"
+    }
+
+    return drawUserTable(userTable)
 }
 
 module.exports = minesweeper
